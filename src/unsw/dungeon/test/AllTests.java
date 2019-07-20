1666d7ebd9af54be95316e70991b8ca668007e01
TimHuang19 <timhuangetg@gmail.com>
2019-07-21 08:10:33 +1000

    goals tests finished

--- src/unsw/dungeon/test/AllTests.java
+++ src/unsw/dungeon/test/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ EnemyBehaviourTests.class, PlayerInteractionTests.class, PlayerMovementTests.class })
+@SuiteClasses({ EnemyBehaviourTests.class, PlayerInteractionTests.class, PlayerMovementTests.class, GoalsTests.class })
 public class AllTests {
 
 }
