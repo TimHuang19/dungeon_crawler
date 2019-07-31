c7216f8f5df405699b1232e86963cc07f9c34a46
Michael Wang <michael.mhw1@gmail.com>
2019-07-31 23:18:38 +1000

    made player image darker so easier to see

--- src/unsw/dungeon/PauseController.java
+++ src/unsw/dungeon/PauseController.java
@@ -2,7 +2,6 @@ package unsw.dungeon;
 
 import java.io.IOException;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
